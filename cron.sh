bb8b8562e4ce8a26367e0f3558834d482827528e
Min RK <benjaminrk@gmail.com>
2019-02-02 22:25:46 +0100

    update echo & force-fix netrc permissions

--- cron.sh
+++ cron.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -eux
-
+test -f ~/.netrc && chmod 0600 ~/.netrc
+set -eu
 d=$(dirname $0)
 cd $d
 date
-
+set -x
 /usr/local/bin/python3 wunderlog.py norway/asker Norway/Akershus/Asker/Asker
 
